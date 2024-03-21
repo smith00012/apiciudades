281f647c1b97a55a681b7bfffb4185fda4bd6911
smith00012 <smithtemoche24@gmail.com>
2024-03-21 16:28:19 -0500

    newconeccion

--- conexion.php
+++ conexion.php
@@ -1,12 +1,19 @@
 <?php
 
-$hostname='localhost';
-$database="turismo_verde";
-$username='root';
-$pass='';
+define('DB_HOST', 'monorail.proxy.rlwy.net');
+define('DB_NAME', 'railway');
+define('DB_USER', 'root');
+define('DB_PASS', 'YpcgaGmMKONOqpRrNYdtHYArxAWfKEEn');
 
-$conexion= new mysqli($hostname,$username,$pass,$database);
-if ($conexion->connect_error){
-    die("Error conexion");
+try {
+    $conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if ($conexion->connect_error) {
+        throw new Exception('Error de conexión: ' . $conexion->connect_error);
+    } else {
+        echo "Conexión exitosa a la base de datos.";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
 ?>
